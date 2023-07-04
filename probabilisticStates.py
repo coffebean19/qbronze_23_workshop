2e64f3b2cc1781cd4747b99392f5ad397e636925
Istvan <istvan.mayer1999@protonmail.com>
2023-07-04 17:18:56 +0200

    Finished CS20.

--- probabilisticStates.py
+++ probabilisticStates.py
@@ -29,3 +29,41 @@ for j in range(3):
 print("matrix A before normalization:")
 for i in range(3):
     print(A[i])
+
+#Normalization
+for j in range(3):
+    for i in range(3):
+        A[i][j] /= norm_factor[j]
+
+#Matrix A after normalization
+print()
+print("Matrix A after normalization")
+for i in range(3):
+    print(A[i])
+
+sum = [0,0,0]
+for j in range(3):
+    for i in range(3):
+        sum[j] += A[i][j]
+
+print(sum)
+
+
+B = [[0.4,0.6,0],
+     [0.2,0.1,0.7],
+     [0.4,0.3,0.3]
+    ]
+#Current state
+v = [0.1,0.3,0.6]
+
+newstate = []
+
+index = 0
+
+for row in B:
+    newstate.append(0)
+    for i in range(len(row)):
+        newstate[index] = newstate[index] + row[i] * v[i]
+    index += 1
+    
+print(newstate)
